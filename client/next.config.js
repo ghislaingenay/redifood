f3ae08f70fd3cef1ff2cee1c5fd646bd354e15ea
ghislaingenay <ghislain.gny@gmail.com>
2023-01-05 12:04:52 +0700

    Create Next.config file

---
+++ client/next.config.js
@@ -0,0 +1,57 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+
+const securityHeaders = [
+    { key: "X-Frame-Options", value: "DENY" },
+    // {
+    //     key: "Content-Security-Policy",
+    //     value: "default-src 'self' https: data: blob: 'unsafe-inline' 'unsafe-eval'",
+    // },
+    {
+        key: "Permissions-Policy",
+        value: "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+    },
+    { key: "Referrer-Policy", value: "origin-when-cross-origin" },
+    { key: "X-Content-Type-Options", value: "nosniff" },
+    {
+        key: "Strict-Transport-Security",
+        value: "max-age=31536000; includeSubDomains; preload",
+    },
+];
+
+const nextConfig = {
+    /* config options here */
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: ["res.cloudinary.com", "images.unsplash.com"],
+    },
+    devIndicators: {
+        buildActivityPosition: "bottom-right",
+    },
+    generateBuildId: async () => {
+        // You can, for example, get the latest git commit hash here
+        return "my-build-id";
+    },
+    poweredByHeader: false,
+    // Use the CDN in production and localhost for development.
+    // assetPrefix: isProd ? 'https://cdn.mydomain.com' : undefined,
+    // async rewrites() {
+    //   return []}
+    compiler: {
+        removeConsole: true,
+    },
+    // apply HTTP response headers to all routes in your application
+
+    async headers() {
+        return [
+            {
+                source: "/:path*",
+                headers: securityHeaders,
+            },
+        ];
+    },
+};
+
+export default nextConfig;
