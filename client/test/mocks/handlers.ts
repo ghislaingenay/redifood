d3bfcac75b7be5c16492628bc10e632ea1adeeec
ghislaingenay <ghislain.gny@gmail.com>
2023-01-07 13:02:57 +0700

    RED1 Add Mocks server for client

---
+++ client/test/mocks/handlers.ts
@@ -0,0 +1,15 @@
+import { rest } from "msw";
+
+export const handlers = [
+  rest.post("http://localhost:3030/api/auth/login", async (req, res, ctx) => {
+    console.log(req);
+    const { username, password }: { username: string; password: string } =
+      await req.json();
+    console.log(username, password);
+    if (username !== "test")
+      return res(ctx.status(401), ctx.json({ message: "Invalid username" }));
+    if (password !== "pit")
+      return res(ctx.status(401), ctx.json({ message: "Invalid password" }));
+    return res(ctx.status(200), ctx.json({ message: "Success" }));
+  }),
+];
