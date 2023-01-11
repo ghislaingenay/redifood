1715891ab0ba596fb21a82a7d7ed1127181a564d
ghislaingenay <ghislain.gny@gmail.com>
2023-01-11 09:54:50 +0700

    RED3 Create error handler middleware from Custom (instanceof CustomError) + Fix async in app routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,9 @@
 import { json } from "body-parser";
 import express from "express";
+import "express-async-errors";
 import { authRouter } from "./controllers/auth.controller";
 import { currentUserRouter } from "./controllers/currentuser.controller";
+import { errorHandler } from "./middlewares/error-handler.mdwr";
 
 const app = express();
 app.use(json());
@@ -9,6 +11,12 @@ app.use(json());
 // routes
 app.use(currentUserRouter);
 app.use(authRouter);
+
+app.all("*", async () => {
+  throw new Error("Route not found");
+});
+
+app.use(errorHandler);
 
 app.listen(3001, () => {
   console.log("Listening on port 3001!");
---
+++ auth/src/middlewares/error-handler.mdwr.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import { CustomError } from "../errors/custom.err";
+
+export const errorHandler = (err: Error, req: Request, res: Response) => {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
+  console.error(err);
+
+  res.status(400).send({ errors: [{ message: "Something went wrong" }] });
+};
