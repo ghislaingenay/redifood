ce33f3786c9690bbdc4807037c2c616763fecf11
ghislaingenay <ghislain.gny@gmail.com>
2023-01-11 11:16:15 +0700

    RED3 Create Password Manager class with hashing/compae using bcrypt

---
+++ auth/src/services/password-manager.ts
@@ -0,0 +1,18 @@
+import bcrypt from "bcrypt";
+// import { randomBytes } from "crypto";
+
+export class PasswordManager {
+  //  Static means that we can use Password.toHash() or Password.compare()
+  // Don't need to create a new instance of it  like const newPassword = new Password()
+  static async toHash(password: string) {
+    // const salt = randomBytes(8).toString("hex");
+    return bcrypt.hashSync(password, 10);
+  }
+
+  static async compare(storedpassword: string, suppliedPassword: string) {
+    // const [hashedPassword, salt] = storedpassword.split(".");
+    await bcrypt.compare(suppliedPassword, storedpassword).then((res: boolean) => {
+      return res;
+    });
+  }
+}
