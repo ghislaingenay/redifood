f9c110a7e42cc7cae85ec9d01b34ca84d7513a29
ghislaingenay <ghislain.gny@gmail.com>
2023-01-11 16:05:36 +0700

    RED3 Pass test for POST /api/auth/signup

--- auth/src/controllers/__test__/auth.test.tsx
+++ auth/src/controllers/__test__/auth.test.tsx
@@ -17,7 +17,7 @@ describe("POST /api/auth/login", () => {
       .post("/api/auth/signup")
       .send({
         username: "test",
-        password: "Fyu89*_vhhvgh",
+        password: "Fyu89*_vhVgh",
       })
       .expect(201);
     await request(app)
@@ -44,6 +44,107 @@ describe("POST /api/auth/login", () => {
         password: "Fyu89*_vhhvgh",
       })
       .expect(200);
+
+    expect(response.get("Set-Cookie")).toBeDefined();
+  });
+});
+
+describe("POST /api/auth/signup", () => {
+  it("returns a 201 on successful signup", async () => {
+    await request(app)
+      .post("/api/auth/signup")
+      .send({
+        username: "test",
+        password: "Fyu89*_vhhvgh",
+      })
+      .expect(201);
+  });
+
+  it("returns a 400 with an invalid username", async () => {
+    await request(app)
+      .post("/api/auth/signup")
+      .send({
+        username: "te",
+        password: "Fyu89*_vhhvgh",
+      })
+      .expect(400);
+  });
+
+  it("returns a 400 with an invalid password", async () => {
+    await request(app)
+      .post("/api/auth/signup")
+      .send({
+        username: "test",
+        password: "Fyu89*_vhhvghFyu89*_vhhvghFyu89*_vhhvghFyu89*_vhhvgh",
+      })
+      .expect(400);
+
+    await request(app)
+      .post("/api/auth/signup")
+      .send({
+        username: "test",
+        password: "Fyu8h",
+      })
+      .expect(400);
+
+    await request(app)
+      .post("/api/auth/signup")
+      .send({
+        username: "test",
+        password: "fyu89*_vhhvgh",
+      })
+      .expect(400);
+    await request(app)
+      .post("/api/auth/signup")
+      .send({
+        username: "test",
+        password: "fyu89vhHvgh",
+      })
+      .expect(400);
+    await request(app)
+      .post("/api/auth/signup")
+      .send({
+        username: "test",
+        password: "fyu*_vHhvgh",
+      })
+      .expect(400);
+    await request(app)
+      .post("/api/auth/signup")
+      .send({
+        username: "test",
+        password: "Fyu89*_vhhvgh",
+      })
+      .expect(201);
+  });
+
+  it("returns a 400 with missing username and password", async () => {
+    return request(app).post("/api/auth/signup").send({}).expect(400);
+  });
+
+  it("disallows duplicate usernames", async () => {
+    await request(app)
+      .post("/api/auth/signup")
+      .send({
+        username: "test",
+        password: "Fyu89*_vhhvgh",
+      })
+      .expect(201);
+    await request(app)
+      .post("/api/auth/signup")
+      .send({
+        username: "test",
+        password: "Fyu89*_vhhvgh",
+      })
+      .expect(400);
+  });
+  it("sets a cookie after successful signup", async () => {
+    const response = await request(app)
+      .post("/api/auth/signup")
+      .send({
+        username: "test",
+        password: "Fyu89*_vhhvgh",
+      })
+      .expect(201);
 
     expect(response.get("Set-Cookie")).toBeDefined();
   });
--- auth/src/services/auth.const.ts
+++ auth/src/services/auth.const.ts
@@ -1,9 +1,16 @@
 import { body } from "express-validator";
 
 export const validationUsers = [
-  body("username").isString().withMessage("username should be defined"),
+  body("username")
+    .isString()
+    .matches(/^[a-zA-Z0-9._-]{4,12}$/)
+    .withMessage("username should be defined"),
   body("password")
     .trim()
-    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%_*#?&]{8,20}/)
+    .matches(/^(?=.{8,20}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[-+_!@#$%^&*.,?]).*$/)
     .withMessage("password must contain at least one uppercase, one lowercase, one number and one special character"),
 ];
+
+// regex that check a password between 8 and 20 characters which contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character
+// /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$/
+// /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[-+_!@#$%^&*.,?])(?=.{8,20}).*$/
