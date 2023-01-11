9c68c09e10a53008b5c5635a6fc642bc71baff6a
ghislaingenay <ghislain.gny@gmail.com>
2023-01-11 11:14:29 +0700

    RED3 Add type checking with TS in user model

--- auth/src/models/users.ts
+++ auth/src/models/users.ts
@@ -1,34 +1,40 @@
 import mongoose from "mongoose";
 
+// what it takes to cerate a user
+interface UserAttrs {
+  username: string;
+  password: string;
+}
+
+// interface that describes the properties the user document has
+interface UserDoc extends mongoose.Document {
+  username: string;
+  password: string;
+  // Add mongoose to add properties for us here
+}
+
+// Interface that describes the properties that a User Model has
+// Tell the interface that it's going to be a build fn
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
   },
   password: {
-    type: ObjectId,
-    ref: "users",
-    required: true,
-  },
-  location: {
-    type: String,
-    required: true,
-  },
-  photos: [String],
-  price: {
-    type: Number,
-    required: true,
-  },
-  rooms: {
-    type: Number,
-    required: true,
-  },
-  title: {
     type: String,
     required: true,
   },
 });
 
-const User = mongoose.model("users", userSchema);
+// Add a static method to the model to check type
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
 
-export default User;
+const User = mongoose.model<UserDoc, UserModel>("users", userSchema);
+
+export { User };
