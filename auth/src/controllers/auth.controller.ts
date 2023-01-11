521e7cc25d0318a1ff363d7647dfd2c47304c4fe
ghislaingenay <ghislain.gny@gmail.com>
2023-01-11 09:40:28 +0700

    RED3 Implement POST /api/auth/signup route

--- auth/src/controllers/auth.controller.ts
+++ auth/src/controllers/auth.controller.ts
@@ -1,11 +1,30 @@
 import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 const router = express.Router();
 
-router.post("/api/auth/login", (req: Request, res: Response) => {
-  res.send("Hi there");
-});
+router.post(
+  "/api/auth/signup",
+  [
+    body("username").isString().withMessage("username should be defined"),
+    body("password")
+      .trim()
+      .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d!@#$%^()&*_]{8,20}$/)
+      .withMessage(
+        "password must contain at least one uppercase, one lowercase, one number, one special character and must be between 8 and 20 characters long",
+      ),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+    const { username, password } = req.body;
+    console.log("Creating a user...", username, password);
+    res.send({});
+  },
+);
 
-router.post("/api/auth/signup", (req: Request, res: Response) => {
+router.post("/api/auth/login", (req: Request, res: Response) => {
   res.send("Hi there");
 });
 
