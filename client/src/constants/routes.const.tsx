4c6246947f9bd0d4487c0e05b995ce3b578b8842
ghislaingenay <ghislain.gny@gmail.com>
2023-01-12 15:57:43 +0700

    RED1 Add routing for Nav in Header of _app.tsx

--- client/src/constants/routes.const.tsx
+++ client/src/constants/routes.const.tsx
@@ -1,10 +1,11 @@
 import { AppleOutlined, AreaChartOutlined, HomeOutlined, PauseOutlined, SettingOutlined } from "@ant-design/icons";
 import { ENavList } from "@interfaces/nav.interface";
 
+const fontSizeIcons = { fontSize: "1rem" };
 export const navRoutes = [
-  { key: ENavList.HOME, icon: <HomeOutlined />, label: ENavList.HOME_LABEL },
-  { key: ENavList.FOOD, icon: <AppleOutlined />, label: ENavList.FOOD_LABEL },
-  { key: ENavList.ANALYTICS, icon: <AreaChartOutlined />, label: ENavList.ANALYTICS_LABEL },
-  { key: ENavList.SETTINGS, icon: <SettingOutlined />, label: ENavList.SETTINGS_LABEL },
-  { key: ENavList.ABOUT_US, icon: <PauseOutlined />, label: ENavList.ABOUT_US_LABEL },
+  { key: ENavList.HOME, icon: <HomeOutlined style={fontSizeIcons} />, label: ENavList.HOME_LABEL },
+  { key: ENavList.FOOD, icon: <AppleOutlined style={fontSizeIcons} />, label: ENavList.FOOD_LABEL },
+  { key: ENavList.ANALYTICS, icon: <AreaChartOutlined style={fontSizeIcons} />, label: ENavList.ANALYTICS_LABEL },
+  { key: ENavList.SETTINGS, icon: <SettingOutlined style={fontSizeIcons} />, label: ENavList.SETTINGS_LABEL },
+  { key: ENavList.ABOUT_US, icon: <PauseOutlined style={fontSizeIcons} />, label: ENavList.ABOUT_US_LABEL },
 ];
