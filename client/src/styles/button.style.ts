e62bfdf8dc1f6109b89324bcad05b6c228e94e36
ghislaingenay <ghislain.gny@gmail.com>
2023-01-13 11:42:15 +0700

    RED1 Fix button styling for Auth login/signup

--- client/src/styles/button.style.ts
+++ client/src/styles/button.style.ts
@@ -3,7 +3,9 @@ import styled from "styled-components";
 
 export const AuthRadioButton = styled(Radio.Button)`
   text-align: center;
-  width: 40vw;
+  justify-content: center;
+  align-items: center;
+  width: 15vw;
   font-size: 1.125rem;
   font-weight: 600;
   font-family: "Bebas Neue", cursive;
