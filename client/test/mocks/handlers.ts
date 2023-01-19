bd48cb3e74b5787e2574b4b6094e4338112a9889
ghislaingenay <ghislain.gny@gmail.com>
2023-01-19 12:21:24 +0700

    RED4 Mock fake data for testing "/"

--- client/test/mocks/handlers.ts
+++ client/test/mocks/handlers.ts
@@ -1,4 +1,5 @@
 import { rest } from "msw";
+import { allDataOrders, getListUnpaidOrders } from "./mocked.data";
 
 export const handlers = [
   rest.post("/api/auth/login", async (req, res, ctx) => {
@@ -19,69 +20,15 @@ export const handlers = [
     }
   }),
 
-  rest.get("/api/orders/all", async (req, res, ctx) => {
+  rest.get("/api/orders", async (req, res, ctx) => {
     const { orderId }: { orderId: string } = await req.json();
     const filter = orderId ? { orderId } : null;
 
-    const data = [
-      {
-        orderId: "APP1",
-        orderStatus: "CREATED",
-        orderDate: "2021-01-01",
-        tableNumber: 2,
-        orderTotal: 100,
-        orderItems: [
-          {
-            foodName: "ITEM1",
-            foodSection: "DRINK",
-            foodExtra: "HOT DRINK",
-            itemQuantity: 1,
-            itemPrice: 100,
-            currency: "USD",
-          },
-        ],
-      },
-      {
-        orderId: "FTP2",
-        orderStatus: "CREATED",
-        orderDate: "2021-04-05",
-        tableNumber: 5,
-        orderTotal: 200,
-        orderItems: [
-          {
-            foodName: "ITEM1",
-            foodSection: "PIK",
-            foodExtra: "MAN Z",
-            itemQuantity: 2,
-            itemPrice: 100,
-            currency: "USD",
-          },
-        ],
-      },
-      {
-        orderId: "KBB3",
-        orderStatus: "CREATED",
-        orderDate: "2021-07-04",
-        tableNumber: 8,
-        orderTotal: 100,
-        orderItems: [
-          {
-            foodName: "ITEM3",
-            foodSection: "DRINK",
-            foodExtra: "SOFT DRINK",
-            itemQuantity: 1,
-            itemPrice: 80,
-            currency: "USD",
-          },
-        ],
-      },
-    ];
-
     if (!filter) {
-      return res(ctx.status(200), ctx.json(data));
+      return res(ctx.status(200), ctx.json({ allDataOrders, getListUnpaidOrders }));
     } else {
-      let res: any = data.find((item) => item.orderId === filter.orderId);
-      return res(ctx.status(200), ctx.json(res));
+      let res: any = allDataOrders.find((item) => item.orderId === filter.orderId);
+      return res(ctx.status(200), ctx.json({ allDataOrders: res, getListUnpaidOrders }));
     }
   }),
 ];
