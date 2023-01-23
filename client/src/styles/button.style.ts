b9d0b2d3559d7e25a2072ce2bfe930c2bb5379f2
ghislaingenay <ghislain.gny@gmail.com>
2023-01-23 15:10:39 +0700

    RED4 Finish Global Button styling

--- client/src/styles/button.style.ts
+++ client/src/styles/button.style.ts
@@ -1,5 +1,13 @@
-import { Radio } from "antd";
+import { Button, Radio } from "antd";
 import styled from "styled-components";
+import {
+  CREATE_COLOR,
+  EDIT_COLOR,
+  ERROR_COLOR,
+  MIDDLE_GREY_COLOR,
+  PRIMARY_COLOR,
+  SUCCESS_COLOR,
+} from "../constants/colors.const";
 
 export const AuthRadioButton = styled(Radio.Button)`
   text-align: center;
@@ -9,4 +17,26 @@ export const AuthRadioButton = styled(Radio.Button)`
   font-size: 1.125rem;
   font-weight: 600;
   font-family: "Bebas Neue", cursive;
+`;
+
+export const EditButton = styled(Button)`
+  background-color: ${EDIT_COLOR};
+`;
+
+export const CreateButton = styled(Button)`
+  background-color: ${CREATE_COLOR};
+`;
+export const ErrorButton = styled(Button)`
+  background-color: ${ERROR_COLOR};
+`;
+export const SuccessButton = styled(Button)`
+  background-color: ${SUCCESS_COLOR};
+`;
+
+export const InfoButton = styled(Button)`
+  background-color: ${MIDDLE_GREY_COLOR};
+  color: white;
+`;
+export const DisplayButton = styled(Button)`
+  background-color: ${PRIMARY_COLOR};
 `;
