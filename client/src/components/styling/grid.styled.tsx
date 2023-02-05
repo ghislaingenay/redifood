35417dcfb82e80928ec4fd73c0b61a34f8b13478
ghislaingenay <ghislain.gny@gmail.com>
2023-02-05 16:17:42 +0700

    HS-3 Create automatic rows with justify property included

--- client/src/components/styling/grid.styled.tsx
+++ client/src/components/styling/grid.styled.tsx
@@ -1,5 +1,13 @@
 import { Row } from "antd";
 
 export const RowCenter = ({ ...props }: any) => {
-  return <Row {...props} justify="center" />;
+  return <Row {...props} align="middle" justify="center" />;
+};
+
+export const RowSpaceAround = ({ ...props }: any) => {
+  return <Row {...props} align="middle" justify="space-around" />;
+};
+
+export const RowSpaceBetween = ({ ...props }: any) => {
+  return <Row {...props} align="middle" justify="space-between" />;
 };
