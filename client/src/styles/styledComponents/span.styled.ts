d5f3ac5ae12d3b453d6d710efbd7fe2e67edd6ca
ghislaingenay <ghislain.gny@gmail.com>
2023-02-05 22:44:01 +0700

    HS-3 Move span to separate folder for better readability

---
+++ client/src/styles/styledComponents/span.styled.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+// SPAN
+export const SpanBlock = styled.span`
+  display: block;
+`;
+
+export const SpanBlockM02Y = styled.span`
+  display: block;
+  margin: 0.2rem auto;
+`;
+
+export const SpanBlockM1Y = styled.span`
+  display: block;
+  margin: 1rem auto;
+`;
+
+export const RedSpan = styled.span`
+  color: red;
+`;
--- client/src/styles/styledComponents/typography.styled.ts
+++ client/src/styles/styledComponents/typography.styled.ts
@@ -1,21 +1,14 @@
 import { Input } from "antd";
 import styled from "styled-components";
 
-// SPAN
-export const SpanBlock = styled.span`
-  display: block;
-`;
-
-export const SpanBlockM02Y = styled.span`
-  display: block;
-  margin: 0.2rem auto;
-`;
-
-export const SpanBlockM1Y = styled.span`
-  display: block;
-  margin: 1rem auto;
-`;
-
 export const RoundedInput = styled(Input)`
   border-radius: 2rem;
+`;
+
+export const LabelFormWhite = styled.label`
+  color: white;
+  font-size: 1.25rem;
+  font-weight: 500;
+  padding-left: 1rem;
+  margin: 0.5rem 0;
 `;
