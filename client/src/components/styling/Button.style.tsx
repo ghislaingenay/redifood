43bfad527a3235cb63560b20c1676f36b97ccac5
ghislaingenay <ghislain.gny@gmail.com>
2023-02-08 22:29:19 +0700

    HS-3 Implement global button with basic styling

---
+++ client/src/components/styling/Button.style.tsx
@@ -0,0 +1,44 @@
+import { Button, ButtonProps } from "antd";
+import { BLUE, GREEN_A, GREY, ORANGE, PURPLE, RED } from "../../constants";
+import { EButtonType } from "../../interfaces";
+
+interface IRediButtonProps extends ButtonProps {
+  buttonType: EButtonType;
+}
+
+const RediButton = ({ buttonType, children, ...props }: IRediButtonProps) => {
+  const handleButtonColor = (buttonColor: IRediButtonProps["buttonType"]) => {
+    const basicStyling = { margin: "1rem 0" };
+    switch (buttonColor) {
+      case EButtonType.CREATE: {
+        return { ...basicStyling, backgroundColor: BLUE };
+      }
+      case EButtonType.DISPLAY: {
+        return { ...basicStyling, backgroundColor: ORANGE };
+      }
+      case EButtonType.EDIT: {
+        return { ...basicStyling, backgroundColor: PURPLE };
+      }
+      case EButtonType.ERROR: {
+        return { ...basicStyling, backgroundColor: RED };
+      }
+      case EButtonType.SUCCESS: {
+        return { ...basicStyling, backgroundColor: GREEN_A };
+      }
+      case EButtonType.INFO: {
+        return { ...basicStyling, backgroundColor: GREY };
+      }
+      case EButtonType.NONE: {
+        return { ...basicStyling };
+      }
+    }
+  };
+
+  return (
+    <Button {...props} type="primary" style={handleButtonColor(buttonType)}>
+      {children}
+    </Button>
+  );
+};
+
+export default RediButton;
--- client/src/styles/styledComponents/button.styled.ts
+++ client/src/styles/styledComponents/button.styled.ts
@@ -1,11 +1,5 @@
 import styled from "styled-components";
-// export const EditButton = styled(Button)`
-//   background-color: ${EDIT_COLOR};
-//   color: white;
 
-// `;
-// padding: 1rem 3rem; <= for RadioButton
-// font-size: 2rem;
 export const RadioButton = styled.button`
   font-size: 1.5rem;
   border-radius: 0.5rem;
@@ -13,20 +7,3 @@ export const RadioButton = styled.button`
   &:hover {
     box-shadow: none;
 `;
-//   background-color: ${CREATE_COLOR};
-//   color: white;
-// `;
-// export const ErrorButton = styled(Button)`
-//   background-color: ${ERROR_COLOR};
-// `;
-// export const SuccessButton = styled(Button)`
-//   background-color: ${SUCCESS_COLOR};
-// `;
-
-// export const InfoButton = styled(Button)`
-//   background-color: ${MIDDLE_GREY_COLOR};
-//   color: white;
-// `;
-// export const DisplayButton = styled(Button)`
-//   background-color: ${PRIMARY_COLOR};
-// `;
