358e9e0c29403e27a6376a894aa7740ad6fdfccb
ghislaingenay <ghislain.gny@gmail.com>
2023-02-12 22:09:57 +0700

    HS-3 Add basic table for orders

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,13 @@
-import { Col, Row, Typography } from "antd";
+import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Col, Row, Space, Table, Typography } from "antd";
 import Head from "next/head";
 import { useRouter } from "next/navigation";
 import { useContext, useEffect, useState } from "react";
-import OrderCard from "../src/components/OrderCard";
-import { RediButton } from "../src/components/RediButton";
 import { RediSelect } from "../src/components/RediSelect";
+import { RediButton } from "../src/components/styling/Button.style";
 import AppContext from "../src/contexts/app.context";
+import { EButtonType, IOrder } from "../src/interfaces";
 import { allDataOrders, getListUnpaidOrders } from "../test/mocks/mockOrdersData";
 
 export const getOptions = (array: string[]) => {
@@ -26,6 +28,34 @@ const AllOrdersPage = ({ allOrders, getList, status }) => {
   const [selectedOption, setSelectedOption] = useState("ALL");
   const [filteredOrders, setFilteredOrders] = useState(allOrders);
   const { Title } = Typography;
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "_id",
+      key: "_id",
+    },
+    {
+      title: "Table",
+      dataIndex: "tableNumber",
+      key: "tableNumber",
+    },
+    {
+      title: "Amount",
+      dataIndex: "orderTotal",
+      key: "orderTotal",
+    },
+    {
+      title: "Action",
+      dataIndex: "_id",
+      key: "_id",
+      render: (item: IOrder) => (
+        <Space>
+          <RediButton buttonType={EButtonType.EDIT}>EDIT</RediButton>
+          <RediButton buttonType={EButtonType.SUCCESS}>PAY</RediButton>
+        </Space>
+      ),
+    },
+  ];
 
   const showProperData = (option) => {
     // replace later by axios get
@@ -62,18 +92,17 @@ const AllOrdersPage = ({ allOrders, getList, status }) => {
           />
         </Col>
         <Col className="text-right" lg={4}>
-          <RediButton
-            typeButton="create"
-            onClick={() => router.push("/orders/create")}
-            title="Create Order"
-            haveIcon={true}
-            shape="round"
-          />
+          <RediButton shape="round" buttonType={EButtonType.CREATE}>
+            <Space>
+              <FontAwesomeIcon icon={faPlusCircle} /> Create Order{" "}
+            </Space>
+          </RediButton>
         </Col>
       </Row>
-      {filteredOrders.map((order) => {
+      <Table columns={columns} dataSource={allOrders} />
+      {/* {filteredOrders.map((order) => {
         return <OrderCard order={order} key={order.orderId} />;
-      })}
+      })} */}
     </>
   );
 };
@@ -81,7 +110,7 @@ const AllOrdersPage = ({ allOrders, getList, status }) => {
 export default AllOrdersPage;
 
 export async function getServerSideProps() {
-  return { props: { allOrders: allDataOrders, getList: getListUnpaidOrders, status: "error" } };
+  return { props: { allOrders: allDataOrders, getList: getListUnpaidOrders, status: "success" } };
   // const url = "/api/orders";
   // await axios
   //   .get(url, { params: { selectedOption: "ALL" } })
