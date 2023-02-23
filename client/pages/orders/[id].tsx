14ccced4c589e2b7f8c5ea49adbb34ec25a50a03
ghislaingenay <ghislain.gny@gmail.com>
2023-02-23 22:56:46 +0700

    RED4- Implement basic UI on the page for '/orders/:id

--- client/pages/orders/[id].tsx
+++ client/pages/orders/[id].tsx
@@ -1,79 +1,114 @@
+import { faCartShopping, faCashRegister, faCreditCard } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Col } from "antd";
 import { useState } from "react";
+
+import { useRouter } from "next/navigation";
+import { RediIconButton } from "../../src/components/styling/Button.style";
 import { RowSpaceBetween } from "../../src/components/styling/grid.styled";
-import { EPaymentType } from "../../src/interfaces";
+import RediRadioButton from "../../src/components/styling/RediRadioButton";
+import { capitalize } from "../../src/functions/global.fn";
+import { EButtonType, EPaymentType } from "../../src/interfaces";
+import { SpacingDiv5X } from "../../src/styles/styledComponents/div.styled";
 import { mockOneOrder } from "../../test/mocks/mockOrdersData";
 
 const CurrentOrder = ({ currentOrder, status }: any) => {
+  const router = useRouter();
   console.log(status);
-
+  const { orderItems, orderTotal, orderId } = currentOrder;
   const [paymentChoice, setPaymentChoice] = useState<EPaymentType | null>(null);
 
+  const isDisabled = paymentChoice === null ? true : false;
+  const radioPaymentOptions = [
+    { label: EPaymentType.CASH, value: EPaymentType.CASH, icon: <FontAwesomeIcon icon={faCashRegister} /> },
+    { label: EPaymentType.CARD, value: EPaymentType.CARD, icon: <FontAwesomeIcon icon={faCreditCard} /> },
+  ];
+
   return (
-    <>
+    <SpacingDiv5X>
       <RowSpaceBetween>
         <Col>Order #</Col>
         <Col>Table</Col>
         <Col>Date</Col>
         <Col>alert</Col>
       </RowSpaceBetween>
-      <table className="table text-center table-striped">
+      <table style={{ margin: "2rem 0", width: "100%" }}>
         <thead>
           <tr>
-            <th scope="col ">Food</th>
-            <th scope="col ">Section</th>
-            <th scope="col ">Extra</th>
-            <th scope="col ">Qty</th>
-            <th scope="col ">Unit cost</th>
-            <th scope="col ">Total</th>
+            <th>Food</th>
+            <th>Section</th>
+            <th>Extra</th>
+            <th>Qty</th>
+            <th>Unit cost</th>
+            <th>Total</th>
           </tr>
         </thead>
         <tbody>
-          {currentOrder.orderItem.map((item, index) => {
+          {orderItems.map((item) => {
+            const { itemName, itemSection, itemExtra, itemQuantity, itemPrice, itemId } = item;
             return (
-              <tr key={index}>
-                <td className="">{item.food.name}</td>
-                <td className="">{item.food.section[0].toUpperCase() + item.food.section.substring(1)}</td>
-                <td className="">{item.food.extra[0].toUpperCase() + item.food.extra.substring(1)}</td>
-                <td className="">{item.qty}</td>
-                <td className="">{item.food.price}</td>
-                <td className="">{(item.food.price * item.qty).toFixed(2)}</td>
+              <tr key={itemId}>
+                <td>{itemName}</td>
+                <td>{capitalize(itemSection)}</td>
+                <td>{capitalize(itemExtra)}</td>
+                <td>{itemQuantity}</td>
+                <td>{itemPrice}</td>
+                <td>{(itemPrice * itemQuantity).toFixed(2)}</td>
               </tr>
             );
           })}
 
-          <tr className="table-info">
-            <td className=""></td>
-            <td className=""></td>
-            <td className=""></td>
-            <td className=""></td>
-            <td className="">Total before VAT</td>
-            <td className="">{currentOrder.orderTotal} $</td>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>Total before VAT</td>
+            <td>{orderTotal} $</td>
           </tr>
-          <tr className="food-card table-warning">
-            <td className=""></td>
-            <td className=""></td>
-            <td className=""></td>
-            <td className=""></td>
-            <td className="">VAT(%)</td>
-            <td className="">200</td>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>VAT(%)</td>
+            <td>200</td>
           </tr>
-          <tr className="food-card table-dark">
-            <td className=""></td>
-            <td className=""></td>
-            <td className=""></td>
-            <td className=""></td>
-            <td className="">Total for payment</td>
-            <td className="">{currentOrder.orderTotal} $</td>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>Total for payment</td>
+            <td>{orderTotal} $</td>
           </tr>
         </tbody>
       </table>
-    </>
+      <RediRadioButton
+        radioGroupName="payment"
+        padding="1rem 1rem"
+        fontSize="1rem"
+        options={radioPaymentOptions}
+        haveIcon="true"
+        setSelectedButton={setPaymentChoice}
+        selectedButton={paymentChoice}
+      />
+      <RediIconButton
+        onClick={() => router.push(`/orders/${orderId}`)}
+        iconFt={faCartShopping}
+        disabled={isDisabled}
+        buttonType={EButtonType.SUCCESS}
+      >
+        PAY
+      </RediIconButton>
+    </SpacingDiv5X>
   );
 };
 
 export default CurrentOrder;
-export async function getServerSideProps(context: any) {
-  const id: string = context.query["id"];
+
+export async function getServerSideProps() {
+  // context: any
+  // const id: string = context.query["id"];
   return { props: { currentOrder: mockOneOrder, status: "success" } };
 }
