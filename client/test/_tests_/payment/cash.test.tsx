2cbe5354516c66bf2aa84ddb10a134cba497a6f8
ghislaingenay <ghislain.gny@gmail.com>
2023-03-01 23:37:39 +0700

    RED4 Work on todo for the payment page (RED5) - on progress

---
+++ client/test/_tests_/payment/cash.test.tsx
@@ -0,0 +1,8 @@
+describe("Cash", () => {
+  it.todo("should display the nine numbers");
+  it.todo("should display an input labelled screen disabled with a null value");
+  it.todo("should display a button labelled clear and confirm");
+  it.todo("should button clear when user input value");
+  it.todo("Show user amount to pay with disabled input");
+  it.todo("should display a button labelled clear and confirm");
+});
