9869431880bffe6b232f9f47200a1afa5d8e6958
ghislaingenay <ghislain.gny@gmail.com>
2023-03-03 22:56:12 +0700

    RED5 TDD Add test for payment by cash (user interaction)

--- client/test/_tests_/payment/cash.test.tsx
+++ client/test/_tests_/payment/cash.test.tsx
@@ -1,7 +1,7 @@
 import { screen } from "@testing-library/react";
 import { render } from "../..";
 import PaymentSystem from "../../../pages/orders/[id]/payment/[paymentChoice]";
-import { findButton } from "../../../src/functions/testhelpers.fn";
+import { clickButton, expectAlertLength, findButton } from "../../../src/functions/testhelpers.fn";
 import { EPaymentType } from "../../../src/interfaces";
 import { mockOneOrder } from "../../mocks/mockOrdersData";
 
@@ -17,7 +17,7 @@ describe("Cash - Unit Testing", () => {
   });
   it("should display the nine numbers", async () => {
     [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].forEach(async (num: number) => {
-      expect(await findButton(`num_${num}`)).toBeInTheDocument();
+      expect(await findButton(`${num}`)).toBeInTheDocument();
     });
   });
 
@@ -55,4 +55,69 @@ describe("Cash - Unit Testing", () => {
   });
 });
 
-describe("Cash - User interaction", () => {});
+describe("Cash - User interaction", () => {
+  it("clear and confirm should be enabled when user click on one button", async () => {
+    render(<PaymentSystem {...paymentCashProps} />);
+    await clickButton(/1/i);
+    expect((await screen.findByRole("textbox", { name: /select amount/i })).ariaValueText).toEqual(1);
+    expect(await findButton(/clear/i)).toBeEnabled();
+    expect(await findButton(/confirm/i)).toBeEnabled();
+  });
+
+  it("user click on one button and clear, confirm and clear must be empty", async () => {
+    render(<PaymentSystem {...paymentCashProps} />);
+    await clickButton(/1/i);
+    expect((await screen.findByRole("textbox", { name: /select amount/i })).ariaValueText).toEqual(1);
+    await clickButton(/4/i);
+    expect((await screen.findByRole("textbox", { name: /select amount/i })).ariaValueText).toEqual(1);
+    expect(await findButton(/clear/i)).toBeEnabled();
+    expect(await findButton(/confirm/i)).toBeEnabled();
+    expect(await findButton(/Finalize payment/i)).toBeDisabled();
+    await clickButton(/clear/i);
+    expect(await findButton(/clear/i)).toBeDisabled();
+    expect(await findButton(/confirm/i)).toBeDisabled();
+    expect(await findButton(/Finalize payment/i)).toBeDisabled();
+  });
+
+  it("user should select 15, confirm and click confirm, select input should be cleared, and confirm, clear disabled", async () => {
+    render(<PaymentSystem {...paymentCashProps} />);
+    await clickButton(/num_4/i);
+    await clickButton(/num_4/i);
+    expect((await screen.findByRole("textbox", { name: /select amount/i })).ariaValueText).toEqual(44);
+    expect((await screen.findByRole("textbox", { name: /selected amount/i })).ariaValueText).toEqual(undefined);
+    await clickButton(/confirm/i);
+    expect((await screen.findByRole("textbox", { name: /select amount/i })).ariaValueText).toBe(undefined);
+    expect(await findButton(/clear/i)).toBeDisabled();
+    expect(await findButton(/confirm/i)).toBeDisabled();
+    expect((await screen.findByRole("textbox", { name: /selected amount/i })).ariaValueText).toEqual(44);
+    expect(await findButton(/Finalize payment/i)).toBeEnabled();
+  });
+
+  it.todo("user should click 10 and confirm, pay button must not appear and alert render", async () => {
+    render(<PaymentSystem {...paymentCashProps} />);
+    await clickButton(/num_1/i);
+    await clickButton(/num_0/i);
+    expect(await findButton(/Finalize payment/i)).toBeDisabled(); // Show also that this is in the DOM
+    expect((await screen.findByRole("textbox", { name: /select amount/i })).ariaValueText).toEqual(20);
+    await clickButton(/confirm/i);
+    expect((await screen.findByRole("textbox", { name: /render money/i })).ariaValueText).toEqual("10.65 $");
+    expect((await screen.findByRole("textbox", { name: /reimbursement/i })).ariaValueText).toEqual("9.35 $");
+    expect(await findButton(/Finalize payment/i)).not.toBeInTheDocument();
+  });
+
+  it("user must be able to click a number 20, confirm, and the value should be displayed accordingly", async () => {
+    render(<PaymentSystem {...paymentCashProps} />);
+    await clickButton(/2/i);
+    await clickButton(/0/i);
+    expect((await screen.findByRole("textbox", { name: /select amount/i })).ariaValueText).toEqual(10);
+    expect(screen.queryByRole("alert")).toBe(null);
+    expect(await findButton(/Finalize payment/i)).toBeDisabled();
+    await clickButton(/confirm/i);
+    expectAlertLength(1);
+    expect(await findButton(/Finalize payment/i)).not.toBeInTheDocument();
+    expect((await screen.findByRole("textbox", { name: /render money/i })).ariaValueText).toEqual("10.65 $");
+    expect((await screen.findByRole("textbox", { name: /reimbursement/i })).ariaValueText).toEqual(
+      /not enough amount/i,
+    );
+  });
+});
