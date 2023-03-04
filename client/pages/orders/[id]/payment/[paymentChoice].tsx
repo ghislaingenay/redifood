3f21878f7225b27665d8efd726cc03ada80493e9
ghislaingenay <ghislain.gny@gmail.com>
2023-03-04 16:10:54 +0700

    RED5 Fix layout calculator

--- client/pages/orders/[id]/payment/[paymentChoice].tsx
+++ client/pages/orders/[id]/payment/[paymentChoice].tsx
@@ -1,41 +1,62 @@
+import { faBan } from "@fortawesome/free-solid-svg-icons";
 import { Col } from "antd";
 import { Else, If, Then } from "react-if";
-import { RowSpaceAround } from "../../../../src/components/styling/grid.styled";
-import { EPaymentType } from "../../../../src/interfaces";
-import { LRoundedInput } from "../../../../src/styles";
+import { RediButton, RediIconButton } from "../../../../src/components/styling/Button.style";
+import { RowCenter, RowCenterSp, RowSpaceAround } from "../../../../src/components/styling/grid.styled";
+import { EButtonType, EPaymentType } from "../../../../src/interfaces";
+import { CenteredTitle, LGCard, LRoundedInput } from "../../../../src/styles";
 import { mockOneOrder } from "../../../../test/mocks/mockOrdersData";
 
 const PaymentSystem = ({ paymentType, currentOrder }) => {
   const { orderTotal } = currentOrder;
   console.log(orderTotal);
+
   return (
     <>
       <If condition={paymentType === EPaymentType.CASH}>
         <Then>
           <RowSpaceAround>
             <Col span={11}>
-              <LRoundedInput readOnly={true} aria-label="select amount" value={1234} />
-              {/*<LGCard>
-<LRoundedInput aria-label='select amount' />
-<RowCenter>
-{[1,2,3,4,5,6,7,8,9].map((num: number) => {
-return <Col span={8}><RediButton></RediButton></Col>})
-<Col span={8}></Col><Col span={8}><RediButton></RediButton></Col><Col span={8}></Col>
-<RowCenterSp>
-</RowCenterSp>
-<RediIconButton >Confirm</RediIconButton>
-<RediIconButton >Clear</RediIconButton>
-</RowCenter>
-</LGCard>
-</Col>
-<Col span={11}>
-<LabelFormBlack>Transaction amount</LabelFormBlack>
-<LRoundedInput aria-label='' value={currentOrder.orderTotal}/>
-<LabelFormBlack>Amount</LabelFormBlack>
-<LRoundedInput aria-label='selected amount' />
+              <LGCard>
+                <RowCenter style={{ marginBottom: "1rem" }}>
+                  <LRoundedInput aria-label="select amount" />
+                </RowCenter>
+                <RowCenter gutter={20}>
+                  {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((num: number) => {
+                    return (
+                      <Col span={8} key={num} style={{ marginBottom: "1.5rem", textAlign: "center" }}>
+                        <RediButton
+                          buttonType={EButtonType.INFO}
+                          style={{ width: "100%", padding: "1rem 3rem", fontSize: "2rem" }}
+                          aria-label={`${String(num)}`}
+                        >
+                          {num}
+                        </RediButton>
+                      </Col>
+                    );
+                  })}
+                  <Col span={8}></Col>
+                  <Col span={8} style={{ marginBottom: "1.5rem", textAlign: "center" }}>
+                    <RediButton buttonType={EButtonType.INFO}>0</RediButton>
+                  </Col>
+                  <Col span={8}></Col>
+                  <RowCenterSp>
+                    <RediButton buttonType={EButtonType.SUCCESS}>Confirm</RediButton>
+                    <RediIconButton buttonType={EButtonType.ERROR} iconFt={faBan}>
+                      Clear
+                    </RediIconButton>
+                  </RowCenterSp>
+                </RowCenter>
+              </LGCard>
+            </Col>
+            <Col span={11}>
+              <CenteredTitle>Transaction amount</CenteredTitle>
+              <LRoundedInput aria-label="" value={currentOrder.orderTotal} />
+              <CenteredTitle>Amount</CenteredTitle>
+              <LRoundedInput aria-label="selected amount" />
 
-<LabelFormBlack>Render</LabelFormBlack>
-<LRoundedInput aria-label='selected amount' /> */}
+              <CenteredTitle>Render</CenteredTitle>
+              <LRoundedInput aria-label="selected amount" />
             </Col>
           </RowSpaceAround>
         </Then>
--- client/src/styles/styledComponents/input.styled.ts
+++ client/src/styles/styledComponents/input.styled.ts
@@ -4,8 +4,9 @@ import { hexToRgba } from "../../functions/global.fn";
 
 export const LRoundedInput = styled.input`
   border-radius: 2rem;
-  padding: 1rem 2rem;
-  width: 100%;
+  padding: 0.5rem 2rem;
+  width: 60%;
+  text-align: center;
   font-size: 2rem;
   text-align: center;
   border: 1px solid ${hexToRgba(GREY, 0.5)};
