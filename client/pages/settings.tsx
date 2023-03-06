326150b9276e30d443ed7a2d12dab68dd7a9daa3
ghislaingenay <ghislain.gny@gmail.com>
2023-03-06 21:36:32 +0700

    RED6 - Improve UI for settings page

--- client/pages/settings.tsx
+++ client/pages/settings.tsx
@@ -1,8 +1,9 @@
-import { Form, Radio, Typography } from "antd";
+import { Col, Form, Radio, Typography } from "antd";
 import { useEffect } from "react";
+import { RowSpaceAround } from "../src/components/styling/grid.styled";
 import { useFood } from "../src/contexts/food.context";
 import { ECurrency } from "../src/interfaces";
-import { LabelFormBlack, RoundedInput } from "../src/styles";
+import { CenteredTitle, NoSpacingDivider, RediDivider, RoundedInput } from "../src/styles";
 const { Title } = Typography;
 
 const Settings = () => {
@@ -36,37 +37,56 @@ const Settings = () => {
   return (
     <>
       <Title level={2}>SETTINGS</Title>
+      <NoSpacingDivider />
+      <CenteredTitle style={{ marginTop: 0 }} level={4}>
+        User Information
+      </CenteredTitle>
+      <NoSpacingDivider />
       <Form
+        style={{ margin: "2rem auto" }}
         form={userForm}
         onValuesChange={(e, all) => {
           console.log(e);
           handleUserInfo(all);
         }}
       >
-        <LabelFormBlack htmlFor="email">Email</LabelFormBlack>
-        <Form.Item name="email" id="email">
-          <RoundedInput type="text" aria-label="email" />
+        <Form.Item name="email" id="email" label="Email">
+          <RoundedInput type="text" aria-label="email" style={{ width: "50%" }} />
         </Form.Item>
       </Form>
+      <RediDivider />
+      <NoSpacingDivider />
+      <CenteredTitle style={{ marginTop: 0 }} level={4}>
+        Global settings
+      </CenteredTitle>
+      <NoSpacingDivider />
       <Form
+        style={{ margin: "2rem 0" }}
         form={settingsForm}
+        layout="horizontal"
         onValuesChange={(e, all) => {
           console.log("all", all);
           handleSettingsInfo(all);
         }}
       >
-        <Form.Item label="Show image" name="haveFoodPicture">
-          <Radio.Group buttonStyle="solid" onChange={(e) => setHaveFoodPicture(e.target.value as boolean)}>
-            <Radio.Button value={true}>Yes</Radio.Button>
-            <Radio.Button value={false}>No</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item label="Show description" name="haveFoodDescription">
-          <Radio.Group buttonStyle="solid" onChange={(e) => setHaveFoodDescription(e.target.value as boolean)}>
-            <Radio.Button value={true}>Yes</Radio.Button>
-            <Radio.Button value={false}>No</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
+        <RowSpaceAround>
+          <Col span={11}>
+            <Form.Item label="Show image" name="haveFoodPicture">
+              <Radio.Group buttonStyle="solid" onChange={(e) => setHaveFoodPicture(e.target.value as boolean)}>
+                <Radio.Button value={true}>Yes</Radio.Button>
+                <Radio.Button value={false}>No</Radio.Button>
+              </Radio.Group>
+            </Form.Item>
+          </Col>
+          <Col span={11}>
+            <Form.Item label="Show description" name="haveFoodDescription">
+              <Radio.Group buttonStyle="solid" onChange={(e) => setHaveFoodDescription(e.target.value as boolean)}>
+                <Radio.Button value={true}>Yes</Radio.Button>
+                <Radio.Button value={false}>No</Radio.Button>
+              </Radio.Group>
+            </Form.Item>
+          </Col>
+        </RowSpaceAround>
       </Form>
     </>
   );
