173ab2cc4df23ab8e6c8c2c5823458a6bab7b384
ghislaingenay <ghislain.gny@gmail.com>
2023-03-06 15:34:55 +0700

    RED6 Move Order History Card to separate component

---
+++ client/src/components/food-order/OrderHistoryCard.tsx
@@ -0,0 +1,48 @@
+import { faList } from "@fortawesome/free-solid-svg-icons";
+import { Col } from "antd";
+import { useRouter } from "next/navigation";
+import { EButtonType, IOrder } from "../../interfaces";
+import { CenteredPBold, LGCard } from "../../styles";
+import { RediIconButton } from "../styling/Button.style";
+import { RowCenter, RowSpaceBetween } from "../styling/grid.styled";
+
+
+interface IOrderHistoryCard {
+  foodOrder: IOrder;
+}
+const OrderHistoryCard = ({ foodOrder }: IOrderHistoryCard) => {
+  const router = useRouter();
+  return (
+    <LGCard>
+      <RowSpaceBetween>
+        <Col span={15}>
+          <RowCenter>
+            <Col span={12}>
+              <CenteredPBold>Order number: {foodOrder?._id}</CenteredPBold>
+            </Col>
+            <Col span={12}>
+              <CenteredPBold>Date: </CenteredPBold>
+            </Col>
+            <Col span={12}>
+              <CenteredPBold>Amount: {foodOrder?.orderTotal}</CenteredPBold>
+            </Col>
+            <Col span={12}>
+              <CenteredPBold>Amount: {foodOrder?.orderCurrency}</CenteredPBold>
+            </Col>
+          </RowCenter>
+        </Col>
+        <Col span={7}>
+          <RediIconButton
+            iconFt={faList}
+            buttonType={EButtonType.EDIT}
+            onClick={() => router.push(`/orders/${foodOrder?._id}`)}
+          >
+            VIEW ORDER
+          </RediIconButton>
+        </Col>
+      </RowSpaceBetween>
+    </LGCard>
+  );
+};
+
+export default OrderHistoryCard;
