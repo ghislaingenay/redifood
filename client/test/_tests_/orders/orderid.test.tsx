5c7003cbdfb5fa16dce5da48cfe8977511258897
ghislaingenay <ghislain.gny@gmail.com>
2023-03-06 15:51:43 +0700

    RED6 Remove paid button if order is paid in orders/id + UI testing

--- client/test/_tests_/orders/orderid.test.tsx
+++ client/test/_tests_/orders/orderid.test.tsx
@@ -1,10 +1,12 @@
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import { render } from "../..";
 import CurrentOrder from "../../../pages/orders/[id]";
-import { clickRadio, findButton, findRadio, findText } from "../../../src/functions/testhelpers.fn";
+import { clickRadio, expectFindText, findButton, findRadio, findText } from "../../../src/functions/testhelpers.fn";
 import { mockOneOrder } from "../../mocks/mockOrdersData";
 jest.mock("next/navigation", () => require("next-router-mock"));
 const successPropsOrderId = { currentOrder: mockOneOrder, status: "success" };
+const mockOrderPaid = { ...mockOneOrder, orderStatus: "COMPLETE" };
+const paidOrderProps = { currentOrder: mockOrderPaid, status: "success" };
 
 describe("Order - Unit Testing", () => {
   it("render without crashing", () => {
@@ -55,6 +57,7 @@ describe("Order - Unit Testing", () => {
     expect(await findRadio(/cash/i)).toBeChecked();
     expect(await findButton(/PAY/i)).toBeEnabled();
   });
+
   it('user selects "cash" and clicks "PAY" button', async () => {
     render(<CurrentOrder {...successPropsOrderId} />);
     await clickRadio(/cash/i);
@@ -64,5 +67,39 @@ describe("Order - Unit Testing", () => {
     await clickRadio(/card/i);
     expect(await findRadio(/cash/i)).not.toBeChecked();
     expect(await findRadio(/card/i)).toBeChecked();
+  });
+});
+
+describe("Order - Paid Order", () => {
+  it("should contains the order list", async () => {
+    render(<CurrentOrder {...paidOrderProps} />);
+    expect(await screen.findAllByRole("row")).toHaveLength(5);
+    expect(await screen.findAllByRole("table")).toHaveLength(1);
+    expect(await screen.findAllByRole("rowgroup")).toHaveLength(2);
+    expect(await screen.findAllByRole("columnheader")).toHaveLength(4);
+    expect(await screen.findAllByRole("gridcell")).toHaveLength(20);
+  });
+  it("should contains the order info", async () => {
+    render(<CurrentOrder {...paidOrderProps} />);
+    [/order #/i, /date/i, /table/i].forEach(async (text: any) => {
+      expect(await findText(text)).toBeInTheDocument();
+    });
+  });
+  it("should display paid order", async () => {
+    render(<CurrentOrder {...paidOrderProps} />);
+    await expectFindText(/paid/i);
+  });
+  it("should not have payment choice", async () => {
+    render(<CurrentOrder {...paidOrderProps} />);
+    await waitFor(() => {
+      expect(screen.queryByRole("radio")).not.toBeInTheDocument();
+    });
+  });
+  it("should not have pay button", async () => {
+    render(<CurrentOrder {...paidOrderProps} />);
+    console.log("hey", successPropsOrderId);
+    await waitFor(() => {
+      expect(screen.queryByRole("button", { name: /pay/i })).not.toBeInTheDocument();
+    });
   });
 });
