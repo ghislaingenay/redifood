f2c4500c90a073d464366220befd766b55422403
ghislaingenay <ghislain.gny@gmail.com>
2023-03-12 12:02:33 +0700

    RED7 Chamge system for langauge functions

--- client/pages/api/build-language.tsx
+++ client/pages/api/build-language.tsx
@@ -1,9 +1,36 @@
-import useLanguage from "./useLanguage";
+import Cookies from "js-cookie";
+import { ELanguage } from "../../src/interfaces";
 
-export const buildLanguage = (locale: string, lang: string) => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const { getCookieInformation } = useLanguage();
+export const encodeCookie = (str: string | ELanguage) => {
+  const buff = Buffer.from(str);
+  return buff.toString("base64");
+};
+
+export const decodeCookie = (str: string | ELanguage) => {
+  const buff = Buffer.from(str);
+  return buff.toString() as ELanguage;
+};
+
+export const getCookieInformation = (lang: string) => {
+  const buff = decodeCookie(lang);
+  let value: ELanguage | undefined = undefined;
+  if (Object.values(ELanguage).includes(buff as ELanguage)) {
+    value = buff as ELanguage | undefined;
+  }
+  return value;
+};
+
+export const setCookieInformation = (lang: ELanguage) => {
+  Cookies.remove("lang");
+  console.log("lang choice", lang);
+  Cookies.set("lang", encodeCookie(lang));
+};
+export const buildLanguage = (appContext: any) => {
+  const locale = appContext.ctx.locale;
+  const lang = appContext.ctx.req.cookies;
+  console.log("val", locale, lang);
   const getLanguage = getCookieInformation(lang);
+
   if (!getLanguage) {
     return locale;
   }
--- client/pages/api/useLanguage.tsx
+++ client/pages/api/useLanguage.tsx
@@ -2,6 +2,7 @@ import Cookies from "js-cookie";
 import { useContext, useEffect, useState } from "react";
 import AppContext from "../../src/contexts/app.context";
 import { ELanguage } from "../../src/interfaces";
+import { decodeCookie, encodeCookie } from "./build-language";
 
 const useLanguage = () => {
   const {
@@ -10,21 +11,11 @@ const useLanguage = () => {
 
   const [languageChoice, setLanguageChoice] = useState<string>(ELanguage.ENGLISH);
 
-  const encodeCookie = (str: string | ELanguage) => {
-    const buff = Buffer.from(str);
-    return buff.toString("base64");
+  const setCookie = () => {
+    Cookies.remove("lang");
+    console.log("lang choice", language);
+    Cookies.set("lang", encodeCookie(language));
   };
-
-  const decodeCookie = (str: string | ELanguage) => {
-    const buff = Buffer.from(str);
-    return buff.toString() as ELanguage;
-  };
-
-  // const setCookie = () => {
-  //   Cookies.remove("lang");
-  //   console.log("lang choice", language);
-  //   Cookies.set("lang", encodeCookie(language));
-  // };
 
   const retrieveCookie = (): ELanguage => {
     const cookieInfo = Cookies.get("lang");
@@ -34,27 +25,12 @@ const useLanguage = () => {
     return undefined;
   };
 
-  const getCookieInformation = (lang: string) => {
-    const buff = Buffer.from(lang).toString();
-    let value: ELanguage | undefined = undefined;
-    if (Object.values(ELanguage).includes(buff as ELanguage)) {
-      value = buff as ELanguage | undefined;
-    }
-    return value;
-  };
-
-  const setCookieInformation = (lang: ELanguage) => {
-    Cookies.remove("lang");
-    console.log("lang choice", language);
-    Cookies.set("lang", encodeCookie(lang));
-  };
-
   useEffect(() => {
     return setLanguageChoice(retrieveCookie());
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [language]);
 
-  return { setCookieInformation, getCookieInformation };
+  return { languageChoice, setCookie, retrieveCookie };
 };
 
 export default useLanguage;
