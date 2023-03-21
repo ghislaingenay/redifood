c313b045a39b74ac4cd323fe28b98a62d461c2b6
ghislaingenay <ghislain.gny@gmail.com>
2023-03-21 12:09:05 +0700

    Add payment type to fix non-passing test in payment.test

--- client/test/_tests_/payment/cash.test.tsx
+++ client/test/_tests_/payment/cash.test.tsx
@@ -1,8 +1,8 @@
 import { screen } from "@testing-library/react";
 import { render } from "../..";
 import PaymentSystem from "../../../pages/orders/[id]/payment/[paymentChoice]";
+import { EPaymentType } from "../../../redifood-module/src/interfaces";
 import { clickButton, expectAlertLength, expectFindText, findButton } from "../../../src/functions/testhelpers.fn";
-import { EPaymentType } from "../../../src/interfaces";
 import { mockOneOrder } from "../../mocks/mockOrdersData";
 
 const paymentCashProps = {
