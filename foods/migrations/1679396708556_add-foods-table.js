bf77554b02666515ba0ec9496e468079076d9262
ghislaingenay <ghislain.gny@gmail.com>
2023-03-22 10:54:38 +0700

    RED10 Create migrate up to create foods, extra and section tables

--- foods/migrations/1679396708556_add-foods-table.js
+++ foods/migrations/1679396708556_add-foods-table.js
@@ -4,30 +4,31 @@ exports.shorthands = undefined;
 
 exports.up = (pgm) => {
   pgm.sql(`
-    CREATE TABLE foods (
-      id SERIAL PRIMARY KEY,
-      item_name VARCHAR(20) NOT NULL,
-      item_description VARCHAR(50) DEFAULT '',
-      item_price INTEGER NOT NULL CHECK (price > 0),
-      item_picture VARCHAR NOT NULL,
-      created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-      section_id INTEGER REFERENCES food_section(id),
-      extra_id INTEGER REFERENCES food_extra(id)
-    )
+  CREATE TABLE food_section (
+    id SERIAL PRIMARY KEY,
+    section_order SMALLINT NOT NULL,
+    section_name VARCHAR(30) NOT NULL,
+    section_description VARCHAR(50) DEFAULT ''
+  );
 
-    CREATE TABLE food_section (
-      id SERIAL PRIMARY KEY,
-      section_order INTEGER DEFAULT 0,
-      section_name VARCHAR(20) NOT NULL,
-      section_description VARCHAR(50) DEFAULT ''
-      )
+  CREATE TABLE food_extra (
+    id SERIAL PRIMARY KEY,
+    extra_name VARCHAR NOT NULL,
+    extra_order SMALLINT NOT NULL,
+    extra_description VARCHAR(50) DEFAULT ''
+  );
 
-    CREATE TABLE food_extra (
-      id SERIAL PRIMARY KEY,
-      extra_name VARCHAR(20) NOT NULL,
-      extra_order INTEGER DEFAULT 0,
-      extra_description VARCHAR(50) DEFAULT ''
-    )
+  CREATE TABLE foods (
+    id SERIAL PRIMARY KEY,
+    item_name VARCHAR(20) NOT NULL,
+    item_description VARCHAR(50) DEFAULT '',
+    item_price NUMERIC NOT NULL CHECK (item_price > 0),
+    item_picture VARCHAR NOT NULL,
+    item_created_at DATE DEFAULT NOW(),
+    section_id INTEGER REFERENCES food_section(id),
+    extra_id INTEGER REFERENCES food_extra(id)
+  )
+
     
     `);
 };
--- foods/src/main.ts
+++ foods/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   console.log('Listening on port 3000');
   await app.listen(3000);
 }
