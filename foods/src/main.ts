9788c7c9001567f51d2ae5f6a53749f2fa304c11
ghislaingenay <ghislain.gny@gmail.com>
2023-03-24 23:34:19 +0700

    RED10-Add nestjs + class vaidator / Pipe packages (npm)

--- foods/src/main.ts
+++ foods/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { pool } from './pool.pg';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(cookieParser());
   await pool
     .connect({
       user: process.env.POSTGRES_USER,
