9eefa2aaaebddfa52d6b7995a4c3b6e9f1b28188
ghislaingenay <ghislain.gny@gmail.com>
2023-03-24 11:32:03 +0700

    RED10 Add close connection and query method to pool (postgres)

--- foods/src/pool.pg.ts
+++ foods/src/pool.pg.ts
@@ -9,6 +9,14 @@ class PGPool {
     this._pool.connect();
     return this._pool.query('SELECT 1 + 1');
   }
+
+  close() {
+    return this._pool.end();
+  }
+
+  query(query: string, params?: any[]) {
+    return this._pool.query(query, params);
+  }
 }
 
 const pool = new PGPool();
