2a647a47452a1cb7d6dab266bf819debf6488343
ghislaingenay <ghislain.gny@gmail.com>
2023-03-27 17:31:04 +0700

    RED 10 Add cookieSession (main.ts + npm packages)

--- foods/src/main.ts
+++ foods/src/main.ts
@@ -1,7 +1,8 @@
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import cookieSession from 'cookie-session';
 import { AppModule } from './app.module';
-import { AllExceptionsFilter } from './handling/catch-all.exception';
+// import { AllExceptionsFilter } from './handling/catch-all.exception';
 import { pool } from './pool.pg';
 
 async function bootstrap() {
@@ -19,7 +20,14 @@ async function bootstrap() {
     });
   console.log('Postgres connected');
   console.log('Listening on port 3000');
-  app.useGlobalFilters(new AllExceptionsFilter(app.get(HttpAdapterHost)));
+  // app.useGlobalFilters(new AllExceptionsFilter(app.get(HttpAdapterHost)));
+  app.set('trust proxy', true);
+  app.use(
+    cookieSession({
+      signed: false,
+      secure: process.env.NODE_ENV !== 'test',
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
