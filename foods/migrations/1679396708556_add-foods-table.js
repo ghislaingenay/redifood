09cce808dd60ec25df654a44ec70fd4740ab5c5a
ghislaingenay <ghislain.gny@gmail.com>
2023-03-28 13:41:26 +0700

    RED 10 Add UNIQUE constraints in DB

--- foods/migrations/1679396708556_add-foods-table.js
+++ foods/migrations/1679396708556_add-foods-table.js
@@ -7,13 +7,13 @@ exports.up = (pgm) => {
   CREATE TABLE food_section (
     id SERIAL PRIMARY KEY,
     section_order SMALLINT AUTO_INCREMENT NOT NULL,
-    section_name VARCHAR(30) NOT NULL,
+    section_name VARCHAR(30) UNIQUE NOT NULL,
     section_description VARCHAR(50) DEFAULT ''
   );
 
   CREATE TABLE food_extra (
     id SERIAL PRIMARY KEY,
-    extra_name VARCHAR(30) NOT NULL,
+    extra_name VARCHAR(30) UNIQUE NOT NULL,
     extra_order SMALLINT AUTO_INCREMENT NOT NULL,
     extra_description VARCHAR(50) DEFAULT '',
     section_id INTEGER REFERENCES food_section(id)
@@ -21,7 +21,7 @@ exports.up = (pgm) => {
 
   CREATE TABLE foods (
     id SERIAL PRIMARY KEY,
-    item_name VARCHAR(20) NOT NULL,
+    item_name VARCHAR(20) UNIQUE NOT NULL,
     item_description VARCHAR(50) DEFAULT '',
     item_price NUMERIC NOT NULL CHECK (item_price > 0),
     item_photo VARCHAR NOT NULL,
