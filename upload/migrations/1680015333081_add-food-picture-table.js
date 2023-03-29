1e76aa889ba232ca031d97e8cd18c53b0b49fd3b
ghislaingenay <ghislain.gny@gmail.com>
2023-03-29 10:22:33 +0700

    RED 11 Fix food_picture table in upload srv

--- upload/migrations/1680015333081_add-food-picture-table.js
+++ upload/migrations/1680015333081_add-food-picture-table.js
@@ -5,11 +5,11 @@ exports.shorthands = undefined;
 exports.up = (pgm) => {
   `CREATE TABLE food_picture (
     id SERIAL PRIMARY KEY,
-    item_id INTEGER NOT NULL,
-    photo_url VARCHAR(255) NOT NULL,
+    item_id INTEGER DEFAULT NULL,
+    photo_url VARCHAR(255) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    photo_activated BOOLEAN DEFAULT TRUE,
-    `;
+    photo_activated BOOLEAN DEFAULT FALSE
+    )`;
 };
 
 exports.down = (pgm) => {
