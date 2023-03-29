fb01f2ecc3a8b1cf1a36b40232932c47bcce180c
ghislaingenay <ghislain.gny@gmail.com>
2023-03-29 15:58:06 +0700

    Fix error related to testing (foods + upload)

---
+++ upload/src/test/jest.setup.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+
+// Hooks that run before evry test
+beforeAll(async () => {});
+
+beforeEach(async () => {});
+
+afterAll(async () => {});
