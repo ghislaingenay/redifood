d7db1187ecad0e928c444c4756620d8867072b95
ghislaingenay <ghislain.gny@gmail.com>
2023-04-03 22:59:09 +0700

    RED12 A Create basic photo created consumer

--- upload/src/events/photo-created-consumer.ts
+++ upload/src/events/photo-created-consumer.ts
@@ -1,9 +1,14 @@
-import { EGroupId, ETopics } from "redifood-module/src/events/subjects.interface";
-import { KafkaConsumer } from "../../../event-bus/redifood-module/src/events/base-consumer";
-import { IPhotoCreatedEvent } from "../../../event-bus/redifood-module/src/events/picture/picture.event";
+import { Message } from "kafkajs";
+import { KafkaConsumer } from "../../redifood-module/src/events/base-consumer";
+import { IPhotoCreatedEvent } from "../../redifood-module/src/events/picture/picture.event";
+import { EGroupId, ETopics } from "../../redifood-module/src/events/subjects.interface";
+// import Upload from "../upload.postgres";
 
 export class PhotoCreatedConsumer extends KafkaConsumer<IPhotoCreatedEvent> {
   topic: ETopics.PICTURE_CREATED = ETopics.PICTURE_CREATED;
   groupId: EGroupId.PICTURE;
-  onMessage;
+  async onMessage(data: IPhotoCreatedEvent["data"], msg: Message) {
+    // const query =
+    // await Upload.createUpload;
+  }
 }
