ff5c38a6311fd72896c11aeaec043e21996eb8f0
ghislaingenay <ghislain.gny@gmail.com>
2023-04-04 21:12:37 +0700

    RED 12A Fix picture created producer

--- event-bus/src/kafka/photo-created-producer.ts
+++ event-bus/src/kafka/photo-created-producer.ts
@@ -1,7 +1,7 @@
-import { PhotoCreatedEvent } from 'redifood-module/src/events/picture/picture-class.event';
+import { IPhotoCreatedEvent } from 'redifood-module/src/events/picture/picture.event';
 import { ETopics } from 'redifood-module/src/interfaces';
 import { KafkaProducer } from './ base-producer';
 
-export class PhotoCreatedProducer extends KafkaProducer<PhotoCreatedEvent> {
+export class PhotoCreatedProducer extends KafkaProducer<IPhotoCreatedEvent> {
   topic: ETopics.PICTURE_CREATED = ETopics.PICTURE_CREATED;
 }
