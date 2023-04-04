6347376128f675c1ffb32e024ed3c2410f39c497
ghislaingenay <ghislain.gny@gmail.com>
2023-04-04 21:34:19 +0700

    MOD Move base producer to module

--- event-bus/src/kafka/photo-created-producer.ts
+++ event-bus/src/kafka/photo-created-producer.ts
@@ -1,6 +1,6 @@
 import { IPhotoCreatedEvent } from 'redifood-module/src/events/picture/picture.event';
 import { ETopics } from 'redifood-module/src/interfaces';
-import { KafkaProducer } from './ base-producer';
+import { KafkaProducer } from '../../redifood-module/src/events/ base-producer';
 
 export class PhotoCreatedProducer extends KafkaProducer<IPhotoCreatedEvent> {
   topic: ETopics.PICTURE_CREATED = ETopics.PICTURE_CREATED;
