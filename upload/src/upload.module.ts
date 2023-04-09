a6ed2329551be5e3d987f0f462a3e17259c6e0ef
ghislaingenay <ghislain.gny@gmail.com>
2023-04-09 21:57:45 +0700

    RED 13 Remove ClientsModule (upload srv have no http and do not emit event)

--- upload/src/upload.module.ts
+++ upload/src/upload.module.ts
@@ -1,20 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-// import { ClientsModule, Transport } from '@nestjs/microservices';
-// import { EGroupId } from 'redifood-module/src/interfaces';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    // ClientsModule.register([
-    //   {
-    //     name: EGroupId.FOOD,
-    //     transport: Transport.TCP,
-    //   },
-    // ]),
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [UploadController],
   providers: [UploadService],
 })
