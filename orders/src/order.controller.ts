0bf8d48083d842b6a05e6e23440d9e6f928a316f
ghislaingenay <ghislain.gny@gmail.com>
2023-04-14 15:16:38 +0700

    RED 14 Add model for foods

--- orders/src/order.controller.ts
+++ orders/src/order.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put } from '@nestjs/common';
 import { OrderService } from './order.service';
 
-@Controller()
+@Controller('api/order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
-  @Get()
-  getHello(): string {
-    return this.orderService.getHello();
+  @EventPattern('food-created')
+  @EventPattern('food-updated')
+  @EventPattern('food-deleted')
+  @Get('paid')
+  getPaidOrder(): string {
+    return '';
   }
+
+  @Get(':id')
+  getOneOrder() {}
+
+  @Post()
+  createOrder() {}
+
+  @Put(':id')
+  editOrder() {}
+
+  @Put(':id/await')
+  setAwaitPayment() {}
+  // should emit an event to payment service by settings the order there and foods
+
+  @Put('id/delete')
+  cancelOrder() {}
+  // should emit an event
 }
