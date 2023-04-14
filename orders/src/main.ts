1e848df1a0b5aa5035b93999666cabdebb609a54
ghislaingenay <ghislain.gny@gmail.com>
2023-04-14 15:27:53 +0700

    RED 14 Add No SQL db to orders srv

--- orders/src/main.ts
+++ orders/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import mongoose from 'mongoose';
 import { OrderModule } from './order.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(OrderModule);
+  if (!process.env.MONGO_URI_ORDERS) {
+    throw new Error('MONGO_URI_ORDERS must be defined');
+  }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
+  }
   await app.listen(3000);
 }
 bootstrap();
--- orders/src/order.module.ts
+++ orders/src/order.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [OrderController],
   providers: [OrderService],
 })
