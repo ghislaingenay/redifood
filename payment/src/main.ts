62faf4b571074a7d4afa5929d146bbf68233ee05
ghislaingenay <ghislain.gny@gmail.com>
2023-04-21 21:08:54 +0700

    RED 15 Change aapp files to payments

--- payment/src/main.ts
+++ payment/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { PaymentModule } from './payments.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(PaymentModule);
   await app.listen(3000);
 }
 bootstrap();
