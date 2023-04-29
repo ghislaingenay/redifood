062747b9838776656d189f3fba3dc450449c64f5
ghislaingenay <ghislain.gny@gmail.com>
2023-04-29 15:07:47 +0700

    Remove old folder - use only data from microservices folder

--- backend/old/src/foods/foods.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FoodController } from './foods.controller';
-import { FoodService } from './foods.service';
-
-@Module({
-  controllers: [FoodController],
-  providers: [FoodService],
-})
-export class FoodModule {}
