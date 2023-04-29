3bd0d73f10e5e7ab915c91030f800d02b05fa367
ghislaingenay <ghislain.gny@gmail.com>
2023-04-29 14:50:58 +0700

    Initialize db and verify environment variables

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import cookieSession from 'cookie-session';
+import { urlencoded } from 'express';
+import mongoose from 'mongoose';
 import { AppModule } from './app.module';
+import { pool } from './pool.pg';
+import { verifyKeys } from './verifykeys';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  verifyKeys();
+
+  app.use(
+    urlencoded({
+      extended: true,
+    }),
+  );
+  app.set('trust proxy', true);
+  app.use(
+    cookieSession({
+      signed: false,
+      secure: process.env.NODE_ENV !== 'test',
+    }),
+  );
+  try {
+    await pool.connect({
+      user: process.env.POSTGRES_USER,
+      host: process.env.POSTGRES_HOST,
+      database: process.env.POSTGRES_DB_NAME,
+      password: process.env.POSTGRES_PASSWORD,
+      port: parseInt(process.env.POSTGRES_PORT),
+    });
+
+    await mongoose.connect(process.env.MONGO_URI);
+    await app.listen(3000);
+  } catch (err) {
+    console.log(err);
+  }
 }
 bootstrap();
