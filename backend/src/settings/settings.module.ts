0eb43a457856f198f761bfcb13a86aa2f5ea0c25
ghislaingenay <ghislain.gny@gmail.com>
2023-05-02 14:56:13 +0700

    Remove guard in settings (issue)

--- backend/src/settings/settings.module.ts
+++ backend/src/settings/settings.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from '../../redifood-module/src/handling-nestjs/auth-guard';
+// import { APP_GUARD } from '@nestjs/core';
+// import { AuthGuard } from '../../redifood-module/src/handling-nestjs/auth-guard';
 import { SettingsController } from './settings.controller';
 import { SettingsService } from './settings.service';
 @Module({
   controllers: [SettingsController],
   providers: [
     SettingsService,
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard,
-    },
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: AuthGuard,
+    // },
   ],
 })
 export class SettingsModule {}
