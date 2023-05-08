1216c0916d3d7ad402adc1bfac1519253e700278
ghislaingenay <ghislain.gny@gmail.com>
2023-05-08 08:56:02 +0700

    Fix db schema for orders

--- backend/migrations/1683200024102_my-first-migration.js
+++ backend/migrations/1683200024102_my-first-migration.js
@@ -34,14 +34,20 @@ exports.up = (pgm) => {
   CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
     order_created DATE NOT NULL DEFAULT NOW(),
-    order_time INT NOT NULL DEFAULT 0,
+    order_table_number INTEGER NOT NULL CHECK (order_table_number > 0),
+    order_time NUMERIC NOT NULL DEFAULT 0,
+    order_status VARCHAR(20) NOT NULL DEFAULT 'created',
     order_total NUMERIC NOT NULL CHECK (order_total > 0),
-    user_id INTEGER REFERENCES users(id)
-    );
+    user_id INTEGER REFERENCES users(id),
+    order_completed DATE NOT NULL DEFAULT NOW(),
+    order_no VARCHAR(20) NOT NULL,
+    order_items VARCHAR NOT NULL DEFAULT ''
+);
 
   CREATE TABLE order_items (
     id SERIAL PRIMARY KEY,
     order_id INTEGER REFERENCES orders(id),
+    user_id INTEGER REFERENCES users(id),
     food_id INTEGER REFERENCES foods(id),
     order_item_status VARCHAR(20) NOT NULL,
     order_item_quantity SMALLINT NOT NULL DEFAULT 0,
