75cdf96a37805c6369959ed6c08dca446a7d0d7b
ghislaingenay <ghislain.gny@gmail.com>
2023-05-09 18:47:07 +0700

    Initialize routes for payments

--- backend/src/payments/payments.controller.ts
+++ backend/src/payments/payments.controller.ts
@@ -1,7 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
+import { User } from 'redifood-module/src/handling-nestjs/user-decorator';
+import { UserPayload } from '../../redifood-module/src/interfaces';
+import { AuthGuard } from '../../src/global/auth-guard';
 import { PaymentsService } from './payments.service';
 
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
+
+  @UseGuards(new AuthGuard())
+  @Get('id')
+  async getPaymentByOrderId(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    orderId: number,
+    @User() user: UserPayload,
+  ) {
+    return await this.paymentsService.getPaymentByOrderId(orderId, user.id);
+  }
+
+  // paidOrder
+  @UseGuards(new AuthGuard())
+  @Post()
+  // Might be obtained from callback of stripe payment
+  async payOrder(@Body(new ValidationPipe()) paymentDto: any) {
+    return await this.paymentsService.payOrder(paymentDto);
+  }
 }
--- backend/src/payments/payments.service.ts
+++ backend/src/payments/payments.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import {
+  IGetServerSideData,
+  UserPayload,
+} from '../../redifood-module/src/interfaces';
 
 @Injectable()
-export class PaymentsService {}
+export class PaymentsService {
+  async getPaymentByOrderId(
+    orderId: number,
+    userId: UserPayload['id'],
+  ): Promise<IGetServerSideData<any>> {
+    console.log(orderId, userId);
+    return { results: 'yes', statusCode: HttpStatus.OK, message: 'recovered' };
+  }
+
+  async payOrder(createPaymentDto: any): Promise<IGetServerSideData<any>> {
+    console.log(createPaymentDto);
+    return { results: 'yes', statusCode: HttpStatus.OK, message: 'recovered' };
+  }
+}
