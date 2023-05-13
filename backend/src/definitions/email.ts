d428302dbee16159a9ddac52faac8b6949509922
ghislaingenay <ghislain.gny@gmail.com>
2023-05-13 23:48:33 +0700

    Implement function in EmailService (on going)

---
+++ backend/src/definitions/email.ts
@@ -0,0 +1,122 @@
+import { NotFoundException } from '@nestjs/common';
+import { MailerSend, Recipient, Sender } from 'mailersend';
+import {
+  ELanguage,
+  TEmailType,
+  UserPayload,
+} from 'redifood-module/src/interfaces';
+import { User } from 'src/models/users.model';
+
+class EmailService {
+  protected lang: ELanguage;
+  protected userId: UserPayload['id'];
+  protected emailType: TEmailType;
+  private client: MailerSend;
+  private sender = new Sender('ghislain@food.com', 'Ghislain'); // (email, 'my name')
+  private recepients: Recipient[];
+
+  constructor(lang: ELanguage, userId: UserPayload['id']) {
+    this.lang = lang;
+    this.userId = userId;
+  }
+
+  private verifyKey() {
+    if (!process.env.MAILSENDER_API_KEY) {
+      throw new NotFoundException();
+    }
+  }
+
+  private initialize() {
+    this.verifyKey();
+    this.client = new MailerSend({
+      apiKey: process.env.MAILSENDER_API_KEY,
+    });
+  }
+
+  private async getUser() {
+    return await User.findById(this.userId);
+  }
+
+  // recipients = [new Recipient('your@client.com', 'Your Client')];
+
+  // personalization = [
+  //   {
+  //     email: 'your@client.com',
+  //     data: {
+  //       test: 'Test Value',
+  //     },
+  //   },
+  // ];
+
+  recipients = [new Recipient('your@client.com', 'Your Client')];
+
+  variables = [
+    {
+      email: 'your@client.com',
+      substitutions: [
+        {
+          var: 'test',
+          value: 'Test Value',
+        },
+      ],
+    },
+  ];
+
+  async sendEmail() {
+    //empty
+  }
+
+  // const emailParams = new EmailParams()
+  //   .setFrom(sentFrom)
+  //   .setTo(recipients)
+  //   .setReplyTo(sentFrom)
+  //   .setVariables(variables)
+  //   .setSubject("Subject, {$test}")
+  //   .setHtml("This is the HTML content, {$test}")
+  //   .setText("This is the text content, {$test}");
+
+  // await mailerSend.email.send(emailParams)
+
+  // const variables = [
+  //   {
+  //     email: "your@client.com",
+  //     substitutions: [
+  //       {
+  //         var: 'test',
+  //         value: 'Test Value'
+  //       }
+  //     ],
+  //   }
+  // ];
+
+  // const emailParams = new EmailParams()
+  //   .setFrom(sentFrom)
+  //   .setTo(recipients)
+  //   .setReplyTo(sentFrom)
+  //   .setVariables(variables)
+  //   .setSubject("Subject, {$test}")
+  //   .setHtml("This is the HTML content, {$test}")
+  //   .setText("This is the text content, {$test}");
+
+  // mailerSend.email.template.single("domain_id")
+  // .then((response) => console.log(response.body))
+  // .catch((error) => console.log(error.body));
+
+  //   onst emailParams = new EmailParams()
+  //   .setFrom(sentFrom)
+  //   .setTo(recipients)
+  //   .setReplyTo(sentFrom)
+  //   .setPersonalization(personalization)
+  //   .setSubject("Subject, {{ test }}")
+  //   .setHtml("This is the HTML content, {{ test }}")
+  //   .setText("This is the text content, {{ test }}");
+
+  // await mailerSend.email.send(emailParams);
+}
+
+export default EmailService;
+
+// 1 - Initial mailSender client
+//
+
+//  Add enum for template name (when created)
