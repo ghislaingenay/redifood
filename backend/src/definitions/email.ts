1680a75f945089928a308c71f2bf5ec26f241aa6
ghislaingenay <ghislain.gny@gmail.com>
2023-05-15 21:11:23 +0700

    Create HTML and complete EmailService

--- backend/src/definitions/email.ts
+++ backend/src/definitions/email.ts
@@ -1,5 +1,5 @@
 import { NotFoundException } from '@nestjs/common';
-import { MailerSend, Recipient, Sender } from 'mailersend';
+import { EmailParams, MailerSend, Recipient, Sender } from 'mailersend';
 
 import {
   ELanguage,
@@ -7,7 +7,7 @@ import {
   UserPayload,
 } from '../../redifood-module/src/interfaces';
 import { Setting, SettingsDoc } from '../../src/models/settings.model';
-import { User, UserDoc } from '../../src/models/users.model';
+import { IUserData, User, UserDoc } from '../../src/models/users.model';
 
 class EmailService {
   protected lang: ELanguage;
@@ -17,11 +17,16 @@ class EmailService {
   private sender = new Sender('ghislain@food.com', 'Ghislain'); // (email, 'my name')
   private recepients: Recipient[];
   private htmlText: string;
-  private userEmail: string;
+  private codePassword!: any;
 
-  constructor(emailType: TEmailType, userId: UserPayload['id']) {
+  constructor(
+    emailType: TEmailType,
+    userId: UserPayload['id'],
+    codePassword?: string,
+  ) {
     this.userId = userId;
     this.emailType = emailType;
+    this.codePassword = codePassword;
   }
 
   private verifyKey() {
@@ -53,7 +58,6 @@ class EmailService {
         `${userData.firstName} ${userData.lastName}`,
       ),
     ];
-    this.userEmail = userData.email;
   }
 
   private async getTemplateText(): Promise<void> {
@@ -64,103 +68,53 @@ class EmailService {
     this.htmlText = (response as any).body as string;
   }
 
-  private async implementVariables(): Promise<{ email: string; substitutions: { var: string; value: string }[] }[]> {}
+  private async createHTML(): Promise<string> {
+    const regex = /{{(.*?)}}/g;
+    const matches = this.htmlText.match(regex);
+    let html: string = this.htmlText;
+    const userData: Omit<IUserData, 'password'> = JSON.parse(
+      JSON.stringify(await this.getUser()),
+    );
+    let updatedData: Record<string, any> = userData;
+    if (this.emailType === 'FORGET_PASSWORD') {
+      updatedData = { ...updatedData, codePassword: this.codePassword };
+    }
+    if (matches) {
+      matches.forEach((match) => {
+        const key = match.replace('{{', '').replace('}}', '');
+        const value =
+          updatedData[key as keyof typeof updatedData] || ('' as any);
+        html = html.replace(match, value);
+      });
+      return html;
+    }
+  }
 
-  protected async sendEmail(): Promise<void> {}
+  private setSubject() {
+    switch (this.emailType) {
+      case 'VALIDATE_EMAIL':
+        return this.lang === ELanguage.ENGLISH
+          ? 'FOOD - Email validation'
+          : 'FOOD - Validation de votre email';
+      default:
+        return this.lang === ELanguage.ENGLISH
+          ? 'FOOD - Reset your password'
+          : 'FOOD - Réinitializer votre mot de passe';
+    }
+  }
+
+  protected async sendEmail(): Promise<void> {
     this.initializeClient();
     await this.createRecipient(); // this.recepients
     await this.getTemplateText(); // this.htmlText
+    const html = await this.createHTML();
+    const emailParams = new EmailParams()
+      .setFrom(this.sender)
+      .setTo(this.recepients)
+      .setSubject(this.setSubject())
+      .setHtml(html);
+    await this.client.email.send(emailParams);
   }
-
-  // private async createEmailParams(): Promise<void> {
-  //   const settingsData = await this.getSettings();
-  //   const emailParams = {
-  //     from: this.sender,
-  //     to: this.recepients,
-  //     subject: 'Subject',
-  //     text: 'Text',
-  //     html: 'HTML',
-  //     variables: [
-  //       {
-  //         email: '  ',
-  //         substitutions: [
-
-  // personalization = [
-  //   {
-  //     email: 'your@client.com',
-  //     data: {
-  //       test: 'Test Value',
-  //     },
-  //   },
-  // ];
-
-  variables = [
-    {
-      email: 'your@client.com',
-      substitutions: [
-        {
-          var: 'test',
-          value: 'Test Value',
-        },
-      ],
-    },
-  ];
-
-  async sendEmail() {
-    //empty
-  }
-
-  // const emailParams = new EmailParams()
-  //   .setFrom(sentFrom)
-  //   .setTo(recipients)
-  //   .setReplyTo(sentFrom)
-  //   .setVariables(variables)
-  //   .setSubject("Subject, {$test}")
-  //   .setHtml("This is the HTML content, {$test}")
-  //   .setText("This is the text content, {$test}");
-
-  // await mailerSend.email.send(emailParams)
-
-  // const variables = [
-  //   {
-  //     email: "your@client.com",
-  //     substitutions: [
-  //       {
-  //         var: 'test',
-  //         value: 'Test Value'
-  //       }
-  //     ],
-  //   }
-  // ];
-
-  // const emailParams = new EmailParams()
-  //   .setFrom(sentFrom)
-  //   .setTo(recipients)
-  //   .setReplyTo(sentFrom)
-  //   .setVariables(variables)
-  //   .setSubject("Subject, {$test}")
-  //   .setHtml("This is the HTML content, {$test}")
-  //   .setText("This is the text content, {$test}");
-
-  // mailerSend.email.template.single("domain_id")
-  // .then((response) => console.log(response.body))
-  // .catch((error) => console.log(error.body));
-
-  //   onst emailParams = new EmailParams()
-  //   .setFrom(sentFrom)
-  //   .setTo(recipients)
-  //   .setReplyTo(sentFrom)
-  //   .setPersonalization(personalization)
-  //   .setSubject("Subject, {{ test }}")
-  //   .setHtml("This is the HTML content, {{ test }}")
-  //   .setText("This is the text content, {{ test }}");
-
-  // await mailerSend.email.send(emailParams);
 }
 
 export default EmailService;
-
-// 1 - Initial mailSender client
-//
-
-//  Add enum for template name (when created)
