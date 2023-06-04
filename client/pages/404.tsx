b10fbc96aa835e8feaed41cc4495c3546704181b
ghislaingenay <ghislain.gny@gmail.com>
2023-06-04 19:57:14 +0700

    Implement basic 404 page

--- client/pages/404.tsx
+++ client/pages/404.tsx
@@ -1,5 +1,22 @@
+import { Col } from "antd";
+import { RowSpaceAround } from "../src/components/styling/grid.styled";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
+import { CenteredTitle } from "../src/styles";
+
 const NotFoundPage = () => {
-  return <div>Page not found</div>;
+  return (
+    <div className="background-auth no-overflow">
+      <RowSpaceAround>
+        <Col span={12}>
+          <FontAwesomeIcon icon={faExclamationTriangle} />
+        </Col>
+        <Col span={12}>
+          <CenteredTitle>This page doesn't exist.Please try again </CenteredTitle>
+        </Col>
+      </RowSpaceAround>
+    </div>
+  );
 };
 
 export default NotFoundPage;
