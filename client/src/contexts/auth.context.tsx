18deb273194169ddb68d52d86aef69253bcf819b
ghislaingenay <ghislain.gny@gmail.com>
2023-06-04 15:38:01 +0700

    Fix verifyUser function

--- client/src/contexts/auth.context.tsx
+++ client/src/contexts/auth.context.tsx
@@ -1,6 +1,7 @@
 import Error, { ErrorProps } from "next/error";
-import React, { useContext, useEffect, useState } from "react";
+import { ReactNode, useContext, useEffect, useState } from "react";
 import { AxiosFunction } from "../../pages/api/axios-request";
+import { UserPayload } from "../../redifood-module/src/interfaces";
 import Auth from "../../src/components/Auth";
 import Loading from "../components/Loading";
 
@@ -9,7 +10,7 @@ export const AuthContext = React.createContext({} as IUseAuth);
 
 interface IUseAuth {
   currentUser: any;
-  verifyUser: () => any;
+  verifyUser: () => Promise<UserPayload | null>;
 }
 
 export function useAuth() {
@@ -24,20 +25,19 @@ export function useAuth() {
 }
 
 interface IAuthProvider {
-  children: React.ReactNode;
+  children: ReactNode;
 }
 
 export function AuthProvider({ children }: IAuthProvider) {
   const [currentUser, setCurrentUser] = useState<IUseAuth["currentUser"]>(null);
   const [spinLoading, setSpinLoading] = useState(true);
 
-  // {id: '642146142709983e1548df1f', email: 'test@test.com', iat: 1685775524}
-
   useEffect(() => {
     if (currentUser) {
       setSpinLoading(false);
       return;
     }
+    setSpinLoading(true);
     console.log("currentUser", currentUser);
     AxiosFunction({
       url: `/api/auth/currentuser`,
@@ -55,13 +55,12 @@ export function AuthProvider({ children }: IAuthProvider) {
       });
   }, [currentUser]);
 
-  const verifyUser = () => {
-    AxiosFunction({ url: "/api/auth/currentuser", method: "get", body: {}, queryParams: {} })
+  const verifyUser = async (): Promise<UserPayload | null> => {
+    return AxiosFunction({ url: "/api/auth/currentuser", method: "get", body: {}, queryParams: {} })
       .then((res) => {
-        return res.data.currentUser;
+        return res.currentUser;
       })
-      .catch((err: ErrorProps) => {
-        console.error(err);
+      .catch(() => {
         return null;
       });
   };
