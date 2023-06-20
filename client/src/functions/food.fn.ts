41711eea1513c87b6e6c6c965bb05b5069dca8a7
ghislaingenay <ghislain.gny@gmail.com>
2023-06-20 15:48:08 +0700

    Move setFoodItemsForDb in separate component

--- client/src/functions/food.fn.ts
+++ client/src/functions/food.fn.ts
@@ -1,11 +1,21 @@
-import { IFoodApi, IFoodSectionList } from "../../redifood-module/src/interfaces";
+import { IFoodApi, IFoodOrder, IFoodSectionList } from "../../redifood-module/src/interfaces";
 
 export const convertFoodToSection = (foodList: IFoodApi[], foodSection: IFoodSectionList[]) => {
   let foodObject: Record<string, number[]> = {};
-  foodSection.forEach(({id, sectionName}) => {
+  foodSection.forEach(({ id, sectionName }) => {
     foodObject[sectionName] = Array.from(
       new Set(foodList.filter((food) => food.sectionId === id).map((food) => food.extraId)),
     );
   });
   return foodObject;
+};
+
+export const setFoodItemsForDb = (foodOrder: IFoodApi[]): IFoodOrder[] => {
+  return [...foodOrder].map(({ itemName, itemQuantity, id }) => {
+    return {
+      itemName,
+      itemQuantity,
+      id,
+    } as IFoodOrder;
+  });
 };
